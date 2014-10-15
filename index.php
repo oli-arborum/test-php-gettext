9fdef03fffc4ad68848dd87c885a6a7e26f39591
Oliver B <oli-arborum@users.noreply.github.com>
2014-10-15 21:23:54 +0200

    added new file to main page

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
  <h1>PHP gettext test</h1>
  <p><a href="index_gt.php">using PHP's gettext module</a></p>
  <p><a href="index_gt_fix.php">using PHP's gettext module with a workaround</a></p>
+ <p><a href="index_php_gt.php">using php-gettext (PHP implementation of gettext)</a></p>
  <p><a href="index_zf.php">using Zend Framework's Translation class</a></p>
  <?php echo date('r'); ?>
 </body>
